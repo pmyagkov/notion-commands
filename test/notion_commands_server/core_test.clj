92576f651e5c23515b052ea3ae7ca2e5074c7497
Pasha Myagkov <pmyagkov@rutube.ru>
2022-07-04 22:37:12 +0300

    Initial commit

---
+++ test/notion_commands_server/core_test.clj
@@ -0,0 +1,7 @@
+(ns notion-commands-server.core-test
+  (:require [clojure.test :refer :all]
+            [notion-commands-server.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
