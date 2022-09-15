ea01e0a9e1dfa405250a40db38e1c1ba849d4e50
Pasha Myagkov <pmyagkov@rutube.ru>
2022-09-16 01:18:50 +0300

    Add deps

--- project.clj
+++ project.clj
@@ -3,7 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [clj-http "3.12.3"]
+                 [org.clojure/data.json "2.4.0"]
+                 [ring/ring-core "1.9.5"]
+                 [ring/ring-jetty-adapter "1.9.5"]
+                 [compojure "1.7.0"]]
   :main ^:skip-aot notion-commands-server.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
